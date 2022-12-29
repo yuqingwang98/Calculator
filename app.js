31004c51dcce252c7459cd754542edf2a4ece3b3
Yuqing Wang <yuqingwang42@gmail.com>
2022-12-29 02:49:47 +0000

    clear button functional

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+document.addEventListener("DOMContentLoaded", function(){
+  // store all components on HTML in the js
+  let clear = document.querySelector("#clear-btn");
+  let equal = document.querySelector(".equal");
+  let decimal = document.querySelector(".decimal");
+
+  let numbers = document.querySelectorAll(".number");
+  let operators = document.querySelectorAll(".operator");
+
+  let previousScreen = document.querySelector(".previous");
+  let currentScreen = document.querySelector(".current");
+
+  // one listener for them all
+  numbers.forEach((number) => number.addEventListener("click", function(e){
+    handleNumber(e.target.textContent)
+    currentScreen.textContent = currentValue;
+}))
+
+  operators.forEach((op) => op.addEventListener("click", function(e){
+    handleOperator(e.target.textContent);
+    previousScreen.textContent = previousValue + " " + operator;
+    currentScreen.textContent = currentValue;
+  }))
+
+  clear.addEventListener('click', function(){
+    previousValue = '';
+    currentValue = '';
+    operator = '';
+    previousScreen.textContent = currentValue;
+    currentScreen.textContent = currentValue;
+  })
+})
+
+function handleNumber(num){
+  if(currentValue.length <= 5){
+    currentValue += num;
+}
+}
+
+function handleOperator(op){
+  operator = op;
+  previousValue = currentValue;
+  currentValue = ''
+}
+
 function add(num1, num2){
   return num1+num2
 }
@@ -17,5 +66,3 @@ function divide(num1, num2){
 function operate(operator, num1, num2){
   return operator(num1, num2)
 }
-
-console.log(operate(add, 1, 12))
